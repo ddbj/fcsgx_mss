e6e9d4d659c2a1a4c2b7b205fdf9fb49c2796d37
tkosuge <tkosuge@nig.ac.jp>
2026-04-06 14:11:02 +0900

    Fixed

--- update_taxonomydb.sh
+++ update_taxonomydb.sh
@@ -3,4 +3,5 @@
 # This script must be run by w3const user.
 # Update ete3 taxonomy database, ~/work-kosuge/etetoolkit/ncbitaxonomy.*
 . /home/w3const/work-kosuge/mypy/bin/activate
-python3 /home/w3const/fcsgx_mss/update_taxonomydb.py
+cd ~/fcsgx_mss
+python3 update_taxonomydb.py
